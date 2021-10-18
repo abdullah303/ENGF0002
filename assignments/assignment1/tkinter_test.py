844737178b13e07fb9bf5bfc1b154ca389ec5a79
abdullah303 <abdullah-ahmed@live.co.uk>
2021-10-18 22:41:10 +0100

    assignments

--- assignments/assignment1/tkinter_test.py
+++ assignments/assignment1/tkinter_test.py
@@ -1,7 +1,10 @@
 from tkinter import *
 
+done = False
 def key(event):
+    global done
     if event.char == 'q':
+        done = True
         rootwindow.destroy()
 
 CANVAS_WIDTH = 300
@@ -9,7 +12,7 @@ CANVAS_HEIGHT = 200
 
 rootwindow = Tk();
 rootwindow.bind_all('<Key>', key)
-canvas = Canvas(rootwindow, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
+canvas = Canvas(rootwindow, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 canvas.pack(side = LEFT, fill=BOTH, expand=TRUE)
 textwidget = canvas.create_text(150, 100, anchor="c")
 version = rootwindow.tk.call("info", "patchlevel")
@@ -17,6 +20,6 @@ canvas.itemconfig(textwidget, text="Success! Tk version: "  + version)
 textwidget2 = canvas.create_text(150, 130, anchor="c")
 canvas.itemconfig(textwidget2, text="Press q to exit")
 
-while True:
+while done == False:
     rootwindow.update()
 
